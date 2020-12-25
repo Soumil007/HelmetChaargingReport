0d3e651db61b62eb37edc55dbf0761f0344ad107
Soumil Bose <samuelroy41@gmail.com>
2020-12-25 12:39:32 +0530

    Few css changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ function Header(){
     return<>
         <div className="header">
             <span className="ReportHeading">Altor Helmet Charge Lifecycle Details</span>
+            <span className="storeName">Office</span>
         </div>
     </>
 }
